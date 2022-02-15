c973e0d3d2fdefb4288898c9f1292c4d5518657b
Juergen Furrer <juergen.furrer@gmail.com>
2022-02-15 23:10:08 +0100

    Add Signatur-field

--- Classes/NewtApi/FieldType.php
+++ Classes/NewtApi/FieldType.php
@@ -6,21 +6,22 @@ namespace Infonique\Newt\NewtApi;
 
 class FieldType
 {
-    const UNKNOWN  = 'unknown';
-    const TEXT     = 'text';
-    const HIDDEN   = 'hidden';
-    const INTEGER  = 'integer';
-    const DECIMAL  = 'decimal';
-    const PASSWORD = 'password';
-    const TEXTAREA = 'textarea';
-    const CHECKBOX = 'checkbox';
-    const DATE     = 'date';
-    const TIME     = 'time';
-    const DATETIME = 'datetime';
-    const SELECT   = 'select';
-    const LABEL    = 'label';
-    const IMAGE    = 'image';
-    const FILE     = 'file';
-    const BARCODE  = 'barcode';
-    const RATING   = 'rating';
+    const UNKNOWN    = 'unknown';
+    const TEXT       = 'text';
+    const HIDDEN     = 'hidden';
+    const INTEGER    = 'integer';
+    const DECIMAL    = 'decimal';
+    const PASSWORD   = 'password';
+    const TEXTAREA   = 'textarea';
+    const CHECKBOX   = 'checkbox';
+    const DATE       = 'date';
+    const TIME       = 'time';
+    const DATETIME   = 'datetime';
+    const SELECT     = 'select';
+    const LABEL      = 'label';
+    const IMAGE      = 'image';
+    const FILE       = 'file';
+    const BARCODE    = 'barcode';
+    const RATING     = 'rating';
+    const SIGNATURE  = 'signature';
 }
